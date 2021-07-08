bc8d2ba9e83a5bc847b5dff0e6b81a6e70548242
Rishabh Goswami <41151591+RishabhGoswami@users.noreply.github.com>
2021-07-08 18:01:48 +0530

    Create Miroor tree .py

---
+++ Miroor tree .py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self,data):
+        self.left=None
+        self.right=None
+        self.nextRight=None
+        self.data=data
+    
+def mirror(root):
+    l=[]
+    l.append(root)
+    while(len(l)>0):
+        a=l[0]
+        l.pop(0)
+        a.left,a.right=a.right,a.left
+        if(a.left):
+            l.append(a.left)
+        if(a.right):
+            l.append(a.right)
+        
+def inorder(root):
+    if(root):
+        print(root.data)
+        inorder(root.left)
+        inorder(root.right)
+        
+if __name__=='__main__':
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(7)
+    root.left.right = Node(6)
+    root.right.left = Node(5)
+    root.right.right = Node(4)
+    print(inorder(root))
+    mirror(root)
+    print(inorder(root))
+    
