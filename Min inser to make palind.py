a68907b7d1546bfe3043417eae0e8161a4c1c922
Rishabh Goswami <41151591+RishabhGoswami@users.noreply.github.com>
2021-07-12 19:09:19 +0530

    Create Min inser to make palind.py

---
+++ Min inser to make palind.py
@@ -0,0 +1,18 @@
+def longests(s1,s2,n,m):
+    t=[[0 for j in range(m+1)] for i in range(n+1)]
+    
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if(s1[i-1]==s2[j-1]):
+                t[i][j]=1+t[i-1][j-1]
+            else:
+                t[i][j]=max(t[i-1][j],t[i][j-1])
+                
+    a=n-t[n][m]
+    return a
+    
+s1='gksrek'
+s2=s1[::-1]
+n=len(s1)
+m=len(s2)
+print(longests(s1,s2,n,m))
