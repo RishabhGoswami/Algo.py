673d25dc03d5d66170f46a65f8c704ac0c9dca2e
Rishabh Goswami <41151591+RishabhGoswami@users.noreply.github.com>
2021-07-15 20:46:43 +0530

    Update Min abs diff of pair.py

--- Min abs diff of pair.py
+++ Min abs diff of pair.py
@@ -1,24 +1,3 @@
-def binary(arr,x):
-    low=0
-    high=len(arr)
-    while(low<=high):
-        mid=(low+high)//2
-        if(arr[mid]<x):
-            low=mid+1
-        elif(arr[mid]>x):
-            high=mid-1
-        else:
-            return mid
-    return -1
-    
-def pairs(arr,x):
-    arr=sorted(arr)
-    for i in range(0,len(arr)):
-        if(binary(arr,arr[i]-x)>=0):
-            return True
-    return False
-    
-    
 def minabs(arr,mins):
     arr=sorted(arr)
     low=0
