3a3b192d0313c09d80b03b65c2eec936dfbc896f
Rishabh Goswami <41151591+RishabhGoswami@users.noreply.github.com>
2021-08-19 21:58:39 +0530

    Update Merge 2 bst.py

--- Merge 2 bst.py
+++ Merge 2 bst.py
@@ -1,66 +1,77 @@
 class newNode:
-    def __init__(self,data):
-        self.left=None
-        self.right=None
-        self.data=data
-    
-def inorder(root):
-    if(root):
-        inorder(root.left)
-        print(root.data)
-        inorder(root.right)
-        
+
+	# Constructor to create a new node
+	def __init__(self, data):
+		self.data = data
+		self.left = None
+		self.right = None
+
 def merge(root1,root2):
     s1=[]
     s2=[]
-    c1=root1
-    c2=root2
-    if(not root1):
-        return inorder(root2)
-    if(not root1):
-        return inorder(root1)
-    while(c1 or s1 or c2 or s2):
-        if(c1 or c2):
-            if(c1):
-                s1.append(c1)
-                c1=c1.left
-            if(c2):
-                s2.append(c2)
-                c2=c2.left
-        else:
-            if(not s1):
-                while(s2):
-                    c2=s2.pop()
-                    c2.left=None
-                    inorder(c2)
-                    return
-            if(not s2):
-                while(s1):
-                    c1=s1.pop()
-                    c1.left=None
-                    inorder(c1)
-                    return
-            c1=s1.pop()
-            c2=s2.pop()
-            if(c1.data<c2.data):
-                print(c1.data)
-                c1=c1.right
-                s2.append(c2)
-                c2=None
-            else:
-                print(c2.data)
-                c2=c2.right
-                s1.append(c1)
-                c1=None
-
+    temp=root1
+    while(temp):
+        s1.append(temp)
+        temp=temp.left
+    temp=root2
+    while(temp):
+        s2.append(temp)
+        temp=temp.left
+    ans=[]
+    while(len(s1)>0 and len(s2)>0):
+        a=s1[-1]
+        b=s2[-1]
         
-if __name__=='__main__':
-    root1 = newNode(30)
-    root1.left = newNode(1)
-    root1.right = newNode(50)
+        if(a.data==b.data):
+            c=s1.pop()
+            ans.append(c.data)
+            if(a.right):
+                t=a.right
+                s1.append(t)
+                while(t.left):
+                    s1.append(t)
+                    t=t.left
+            c=s2.pop()
+            if(b.right):
+                t=b.right
+                s2.append(t)
+                while(t.left):
+                    s2.append(t)
+                    t=t.left
+        elif(a.data<=b.data):
+            c=s1.pop()
+            ans.append(c.data)
+            if(a.right):
+                t=a.right
+                s1.append(t)
+                while(t.left):
+                    s1.append(t)
+                    t=t.left
+        else:
+            c=s2.pop()
+            ans.append(c.data)
+            if(b.right):
+                t=b.right
+                s2.append(t)
+                while(t.left):
+                    s2.append(t)
+                    t=t.left
+    print(ans)
     
-    root2 = newNode(4)
-    root2.left = newNode(2)
-    root2.right = newNode(6)
-    
-    merge(root1,root2)
+if __name__ == '__main__':
+	
+	# Let us construct the BST shown in
+	# the above figure
+	root1 = newNode(30)
+	root1.left = newNode(17)
+	root1.right = newNode(40)
+	root1.left.left = newNode(15)
+	root1.left.right = newNode(20)
+	
+	root2 = newNode(24)
+	root2.left = newNode(16)
+	root2.right = newNode(29)
+	root2.left.right = newNode(18)
+	root2.right.right = newNode(30)
+	
+	merge(root1,root2)
