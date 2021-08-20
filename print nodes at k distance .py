db51b0692339eb75e657b3e7315748ac891a28e9
Rishabh Goswami <41151591+RishabhGoswami@users.noreply.github.com>
2021-08-20 23:51:53 +0530

    Update print nodes at k distance .py

--- print nodes at k distance .py
+++ print nodes at k distance .py
@@ -3,44 +3,56 @@ class Node:
         self.data=data
         self.left=None
         self.right=None
-      
-def printk(root,k,level):
+def printdown(root,k):
     if(root):
-        if(level==k):
+        if(k==0):
             print(root.data)
-        printk(root.left,k,level+1)
-        printk(root.right,k,level+1)
+        printdown(root.left,k-1)
+        printdown(root.right,k-1)
 
-def printl(root,target,k):
-    if (root is None):
+def down(root,target,k):
+    if(root is None):
         return -1
-    if(root==target):
-        printk(root,k,0)
-        return 0
-    
-    l=printl(root.left,target,k)
-    if(l!=-1):
-        if(l+1==k):
-            print(root.data)
-        else:
-            printk(root.right,k-l-2,0)
-        return 1+l
-    r=printl(root.right,target,k)
-    
-    if(r!=-1):
-        if(r+1==k):
-            print(root.data)
-        else:
-            printk(root.left,k-r-2,0)
-        return 1+r
-    return -1
         
-root = Node(20)
-root.left = Node(8)
-root.right = Node(22)
-root.left.left = Node(4)
-root.left.right = Node(12)
-root.left.right.left = Node(10)
-root.left.right.right = Node(14)
-target=root.left.right
-printl(root,target,2)
+    if(root==target):
+        printdown(root,k)
+        return 0
+        
+    d1=down(root.left,target,k)
+    if(d1!=-1):
+        if(1+d1==k):
+            print(root.data)
+        else:
+            printdown(root.right,k-d1-2)
+        return 1+d1
+        
+    d2=down(root.right,target,k)
+    if(d2!=-1):
+        if(1+d2==k):
+            print(root.data)
+        else:
+            printdown(root.left,k-d2-2)
+        return 1+d2
+    return -1
+
+if __name__=='__main__':
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+    root.right.left.right = Node(8)
+    target = root.left.right
+    down(root,target,2)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
