37be1e17da563765cab2cc85b3b8bfe0e522fe64
Rishabh Goswami <41151591+RishabhGoswami@users.noreply.github.com>
2021-08-23 00:37:54 +0530

    Update Joseph.py

--- Joseph.py
+++ Joseph.py
@@ -1,9 +1,8 @@
-def joseph(n,k):
-    if(n==1):
-        return 0
-    a=joseph(n-1,k)
-    b=(a+k)%n
-    return b
-    
-print(joseph(7,3))
-    
+def josephus(n, k):
+	if (n == 1):
+		return 1
+	else:
+		return (josephus(n - 1, k) + k-1) % n + 1
+n = 14
+k = 2
+print("The chosen place is ", josephus(n, k))
